8bb81d3a382a88eefdfe43b3c043afa321c69f47
kim-jingyu <swc05161@gmail.com>
2024-02-22 17:49:02 +0900

    feat: blob test

---
+++ blob/blob.php
@@ -0,0 +1,26 @@
+<?php
+    $dbName = getenv("DB_NAME");
+    $dbHost = getenv("DB_HOST");
+    $dbUsername = getenv("DB_USERNAME");
+    $dbPassword = getenv("DB_PASSWORD");
+    
+    $conn = mysqli_connect($dbHost, $dbUsername, $dbPassword, $dbName);
+    if ($conn->errno) {
+        die("DB Down");
+    }
+
+    $file = $_FILES['imageFile'];
+    if (!$file) {
+        die("파일없음");
+    }
+
+    $name = $file['name'];
+    $data = $file['tmp_name'];
+    $type = $file['type'];
+
+    $sql = "INSERT INTO images(name, data, type) VALUES($name, $data, $type)";
+    $res = $conn->query($sql);
+    if(!$res) {
+        die("파일 저장 실패");
+    }
+?>
